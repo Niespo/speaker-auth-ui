d993fd180e0b78a48f387f4d48606b3988437187
Marcin Milewicz <inspiration075@gmail.com>
2015-12-06 16:29:35 +0100

    soundRecorderFactory created

---
+++ www/js/components/sound-recorder/sound-recorder.factory.js
@@ -0,0 +1,27 @@
+(function (global) {
+
+    global
+        .angular
+        .module('app')
+        .factory('soundRecorderFactory', function () {
+            const DEFAULT_DURATION = 10;
+            return {
+                newInstance: newInstance
+            };
+
+            function newInstance(duration) {
+                duration = duration | DEFAULT_DURATION;
+                return {
+                    record: record
+                };
+
+                function record() {
+                    return new Promise(function (resolve, reject) {
+                        navigator.device.capture
+                            .captureAudio(resolve, reject, {duration: duration});
+                    });
+                }
+            }
+
+        });
+})(this);
